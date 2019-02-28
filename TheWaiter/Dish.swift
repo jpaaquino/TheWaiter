9d3a17e2c495592e7e39c2983fd43b9e5268e9fd
João Aquino <jpaaquino@hotmail.com>
2019-02-28 15:10:51 -0300

    first commit

---
+++ TheWaiter/Dish.swift
@@ -0,0 +1,9 @@
+//
+//  Dish.swift
+//  TheWaiter
+//
+//  Created by João Aquino on 28/02/2019.
+//  Copyright © 2019 João Aquino. All rights reserved.
+//
+
+import Foundation
---
+++ TheWaiter/MenuViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MenuViewController.swift
+//  TheWaiter
+//
+//  Created by João Aquino on 28/02/2019.
+//  Copyright © 2019 João Aquino. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
