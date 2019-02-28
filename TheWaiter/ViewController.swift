70e256c1cf87951935676b0b718661bce262f562
João Aquino <joao.aquino@gingaone.com>
2019-02-28 13:43:38 -0300

    Initial Commit

---
+++ TheWaiter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheWaiter
+//
+//  Created by João Aquino on 28/02/2019.
+//  Copyright © 2019 João Aquino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
